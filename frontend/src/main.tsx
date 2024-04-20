3e4c702b3d3505c5cae5a9b6a1fdba44954e6f6c
Wellington Rodrigues <sousa_wellington@outlook.com>
2024-04-20 19:47:27 -0300

    feat: setup to URL State

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import { QueryClientProvider } from "@tanstack/react-query";
 import "../src/styles/globals.sass"
 import { queryClient } from './services/queryClient.ts';
-
+import {
+  RouterProvider,
+} from "react-router-dom";
+import { router } from './routes.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}> {/* use react-query to cache the api requests */}
-      <App />
+      <RouterProvider router={router}/>
     </QueryClientProvider>
   </React.StrictMode>,
 )
