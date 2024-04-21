66dbfe96d3398b75fd5adf757603ec6bae520442
Wellington Rodrigues <sousa_wellington@outlook.com>
2024-04-20 22:10:08 -0300

    feat: revert file to use useState

--- frontend/src/components/FontSizeChanger/index.tsx
+++ frontend/src/components/FontSizeChanger/index.tsx
@@ -1,31 +1,33 @@
+import { useState } from "react";
 import styles from "./styles.module.sass";
 
+
 export function FontSizeChanger() {
+  const [selectedSize, setSelectedSize] = useState(localStorage.getItem('fontSize') || '16px');
+
   const changeFontSize = (size: string, event: React.MouseEvent<HTMLAnchorElement>) => {
     event.preventDefault();
     localStorage.setItem('fontSize', size);
     document.body.style.setProperty('--base-font-size', size);
-
-    // Remove the selected class from all elements
-    document.querySelectorAll(`.${styles.FontSize} a`).forEach(el => {
-      el.classList.remove(styles.Selected);
-    });
-    
-    // Add the selected class to the clicked element
-    event.currentTarget.classList.add(styles.Selected);
+    setSelectedSize(size); 
   };
+
+  const fontSizeIsSelected = (size: string) => selectedSize === size ? styles.Selected : '';
+
+  const classNames = (size: string, cssClass: string) => `${fontSizeIsSelected(size)} ${cssClass}`;
+
 
   return (
     <div className={styles.FontSize}>
-      <a className={`${styles.Small}`} href="#" onClick={(e) => changeFontSize('10px', e)} aria-label="Pequena" title="Pequena">
+      <a className={classNames('10px', styles.Small)} href="#" onClick={(e) => changeFontSize('10px', e)} aria-label="Pequena" title="Pequena">
         A
       </a>
 
-      <a className={`${styles.Medium}`} href="#" onClick={(e) => changeFontSize('16px', e)} aria-label="Normal" title="Normal">
+      <a className={classNames('16px', styles.Medium)} href="#" onClick={(e) => changeFontSize('16px', e)} aria-label="Normal" title="Normal">
         A
       </a>
 
-      <a className={`${styles.Large}`} href="#" onClick={(e) => changeFontSize('22px', e)} aria-label="Grande" title="Grande">
+      <a className={classNames('22px', styles.Large)} href="#" onClick={(e) => changeFontSize('22px', e)} aria-label="Grande" title="Grande">
         A
       </a>
 
